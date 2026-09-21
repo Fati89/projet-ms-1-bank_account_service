3a09ff09da6496f0cfe9ee04b760bbbef8eea4f9
Fati89 <fatimazahraee989@gmail.com>
2026-09-21 22:44:59 +0100

    projection

--- src/main/java/org/sid/bank_account_service/web/BankAccountRestController.java
+++ src/main/java/org/sid/bank_account_service/web/BankAccountRestController.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api")
 public class BankAccountRestController {
 
     private final BankAccountRepository bankAccountRepository;
